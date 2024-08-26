0b22f64860a17d4e5af5fb4441db26506e2b4e82
Thalita Policarpo <thalita_policarpo@outlook.com>
2024-08-25 21:03:38 -0300

    implementação das anotações do Swagger/OpenAPI

--- InspecoesPCI-PB/src/main/java/br/com/edu/infnet/inspecoespcipb/controller/ExtintorController.java
+++ InspecoesPCI-PB/src/main/java/br/com/edu/infnet/inspecoespcipb/controller/ExtintorController.java
@@ -4,6 +4,9 @@ import br.com.edu.infnet.inspecoespcipb.domain.Extintor;
 import br.com.edu.infnet.inspecoespcipb.domain.ExtintorHistorico;
 import br.com.edu.infnet.inspecoespcipb.dto.ExtintorDTO;
 import br.com.edu.infnet.inspecoespcipb.service.ExtintorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,16 +24,26 @@ public class ExtintorController {
     private ExtintorService extintorService;
 
     @GetMapping
+    @Operation(summary = "Obter todos os extintores", description = "Retorna uma coleção de todos os extintores")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Operação bem-sucedida"),
+            @ApiResponse(responseCode = "404", description = "Nenhum extintor encontrado")
+    })
     public ResponseEntity<?> getAll() {
         try {
             Collection<Extintor> extintores = extintorService.getAll();
             return new ResponseEntity<>(extintores, HttpStatus.OK);
         } catch (IllegalArgumentException ex) {
-            return new ResponseEntity<>(ex.getMessage(), HttpStatus.OK);
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
         }
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Obter extintor por ID", description = "Retorna um extintor pelo seu ID")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Operação bem-sucedida"),
+            @ApiResponse(responseCode = "404", description = "Extintor não encontrado")
+    })
     public ResponseEntity<?> getById(@PathVariable int id) {
         try {
             Extintor extintor = extintorService.getById(id);
@@ -41,6 +54,11 @@ public class ExtintorController {
     }
 
     @GetMapping("/controleInterno/{numeroCOntroleInterno}")
+    @Operation(summary = "Obter extintor por número de controle interno", description = "Retorna um extintor pelo seu número de controle interno")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Operação bem-sucedida"),
+            @ApiResponse(responseCode = "404", description = "Extintor não encontrado")
+    })
     public ResponseEntity<?> getByNumeroControleInterno(@PathVariable int numeroCOntroleInterno) {
         try {
             Extintor extintor = extintorService.getByNumeroControleInterno(numeroCOntroleInterno);
@@ -51,6 +69,11 @@ public class ExtintorController {
     }
 
     @PostMapping
+    @Operation(summary = "Salvar um novo extintor", description = "Adiciona um novo extintor")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Extintor incluído com sucesso"),
+            @ApiResponse(responseCode = "409", description = "Conflito ao incluir extintor")
+    })
     public ResponseEntity<String> save(@RequestBody ExtintorDTO extintorDTO) {
         try {
             extintorService.add(extintorDTO);
@@ -61,6 +84,11 @@ public class ExtintorController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Excluir extintor por ID", description = "Exclui um extintor pelo seu ID")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Extintor excluído com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Extintor não encontrado")
+    })
     public ResponseEntity<String> deleteById(@PathVariable int id) {
         try {
             extintorService.deleteById(id);
@@ -71,6 +99,11 @@ public class ExtintorController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Atualizar extintor por ID", description = "Atualiza um extintor pelo seu ID")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Extintor atualizado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Extintor não encontrado")
+    })
     public ResponseEntity<String> update(@PathVariable int id, @RequestBody ExtintorDTO extintorDTO) {
         try {
             extintorService.update(id, extintorDTO);
@@ -81,6 +114,12 @@ public class ExtintorController {
     }
 
     @GetMapping("/historico/{numeroControleInterno}")
+    @Operation(summary = "Obter histórico de extintor por número de controle interno", description = "Retorna o histórico de um extintor pelo seu número de controle interno")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Operação bem-sucedida"),
+            @ApiResponse(responseCode = "404", description = "Nenhum histórico encontrado para o número de controle interno"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     public ResponseEntity<?> getHistoricoByNumeroControleInterno(@PathVariable int numeroControleInterno) {
         try {
             List<ExtintorHistorico> historico = extintorService.getHistoricoByNumeroControleInterno(numeroControleInterno);
--- InspecoesPCI-PB/src/main/java/br/com/edu/infnet/inspecoespcipb/controller/InspecaoExtintorController.java
+++ InspecoesPCI-PB/src/main/java/br/com/edu/infnet/inspecoespcipb/controller/InspecaoExtintorController.java
@@ -3,6 +3,9 @@ package br.com.edu.infnet.inspecoespcipb.controller;
 import br.com.edu.infnet.inspecoespcipb.domain.InspecaoExtintor;
 import br.com.edu.infnet.inspecoespcipb.dto.InspecaoExtintorDTO;
 import br.com.edu.infnet.inspecoespcipb.service.InspecaoExtintorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +21,11 @@ public class InspecaoExtintorController {
     private InspecaoExtintorService inspecaoExtintorService;
 
     @GetMapping
+    @Operation(summary = "Obter todas as inspeções", description = "Retorna uma coleção de todas as inspeções")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Operação bem-sucedida"),
+            @ApiResponse(responseCode = "400", description = "Requisição inválida")
+    })
     public ResponseEntity<?> getAll() {
         try {
             Collection<InspecaoExtintor> inspecoes = inspecaoExtintorService.getAll();
@@ -28,6 +36,11 @@ public class InspecaoExtintorController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Obter inspeção por ID", description = "Retorna uma inspeção pelo seu ID")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Operação bem-sucedida"),
+            @ApiResponse(responseCode = "404", description = "Inspeção não encontrada")
+    })
     public ResponseEntity<?> getById(@PathVariable int id) {
         try {
             InspecaoExtintor inspecao = inspecaoExtintorService.getById(id);
@@ -38,6 +51,11 @@ public class InspecaoExtintorController {
     }
 
     @GetMapping("/extintor/{idExtintor}")
+    @Operation(summary = "Obter inspeções por ID de extintor", description = "Retorna uma coleção de inspeções relacionadas a um extintor específico")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Operação bem-sucedida"),
+            @ApiResponse(responseCode = "404", description = "Inspeções não encontradas para o extintor")
+    })
     public ResponseEntity<?> getByExtintorId(@PathVariable int idExtintor) {
         try {
             Collection<InspecaoExtintor> inspecoes = inspecaoExtintorService.getByExtintorId(idExtintor);
@@ -48,6 +66,11 @@ public class InspecaoExtintorController {
     }
 
     @PostMapping
+    @Operation(summary = "Salvar uma nova inspeção", description = "Adiciona uma nova inspeção")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Inspeção incluída com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Requisição inválida")
+    })
     public ResponseEntity<?> save(@RequestBody InspecaoExtintorDTO inspecaoExtintorDTO) {
         try {
             inspecaoExtintorService.add(inspecaoExtintorDTO);
@@ -58,6 +81,11 @@ public class InspecaoExtintorController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Excluir inspeção por ID", description = "Exclui uma inspeção pelo seu ID")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Inspeção excluída com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Inspeção não encontrada")
+    })
     public ResponseEntity<?> delete(@PathVariable int id) {
         try {
             inspecaoExtintorService.delete(id);
@@ -68,6 +96,11 @@ public class InspecaoExtintorController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Atualizar inspeção por ID", description = "Atualiza uma inspeção pelo seu ID")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Inspeção atualizada com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Inspeção não encontrada")
+    })
     public ResponseEntity<?> update(@PathVariable int id, @RequestBody InspecaoExtintorDTO inspecaoExtintorDTO) {
         try {
             inspecaoExtintorService.update(id, inspecaoExtintorDTO);
